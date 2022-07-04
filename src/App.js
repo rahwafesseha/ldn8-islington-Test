5b02b36e6e622fcbf9105fe0b37d320f9f79dbc3
teniolao <Tennybetty100@gmail.com>
2022-07-04 19:38:32 +0100

    install and implement browser router
    install browser router, create two paths; for home and quiz component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import "./App.css";
 import Header from "./component/header/Header";
 import Home from "./component/home/Home";
+import Quiz from "./component/home/quiz/Quiz";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
+    <BrowserRouter>
       <Header />
-      <Home />
-    </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/quiz" element={<Quiz />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
