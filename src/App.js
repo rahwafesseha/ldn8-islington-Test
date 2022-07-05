b93497542f3c54bb08963e83debd41fdc936b0d7
teniolao <Tennybetty100@gmail.com>
2022-07-05 14:48:10 +0100

    add functionality
    create state to target event for category and difficulty.
    add handle submit function to start quiz button => this takes you to the quiz page

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, { useState } from "react";
 import "./App.css";
 import Header from "./component/header/Header";
 import Home from "./component/home/Home";
@@ -5,11 +6,12 @@ import Quiz from "./component/quiz/Quiz";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
+  const [name, setName] = useState("");
   return (
     <BrowserRouter>
       <Header />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Home name={name} setName={setName}/>} />
         <Route path="/quiz" element={<Quiz />} />
       </Routes>
     </BrowserRouter>
