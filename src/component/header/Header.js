399ada4d2032b5a50ed0aa38f8576f8a3513436b
teniolao <Tennybetty100@gmail.com>
2022-07-06 18:32:34 +0100

    remove category textfield and navigate

--- src/component/header/Header.js
+++ src/component/header/Header.js
@@ -4,7 +4,7 @@ import "./Header.css";
 const Header = () => {
   return (
     <div className="header">
-      <h1>Islington Quiz App</h1>
+      <h1>Islington Learning App</h1>
     </div>
   );
 };
